f0f01b1ef1a334d61ef1a361d75c6d117039dadf
Schuster <Peter_Schuster@cable.comcast.com>
2015-06-13 16:28:29 -0400

    adds data schema for product

--- app/models/product.js
+++ app/models/product.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  name: DS.attr('string'),
+  sku: DS.attr('string'),
+  imageThumbnail: DS.attr('string'),
+  imageMedium: DS.attr('string'),
+  imageDetail: DS.attr('string'),
+  inCart: DS.attr('boolean')
 });
