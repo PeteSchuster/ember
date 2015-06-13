d7a6f572d8dda0310495f14f7789dd9c9b1e4150
Schuster <Peter_Schuster@cable.comcast.com>
2015-06-13 16:44:36 -0400

    adds sample data

--- app/models/product.js
+++ app/models/product.js
@@ -7,4 +7,107 @@ export default DS.Model.extend({
   imageMedium: DS.attr('string'),
   imageDetail: DS.attr('string'),
   inCart: DS.attr('boolean')
+}).reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      'sku': '01',
+      'name': 'Apple',
+      'category': 'fruit',
+      'imageThumbnail': 'http://placehold.it/32x32',
+      'imageMedium': 'http://placehold.it/300x200',
+      'imageDetail': 'http://placehold.it/600x400',
+      'price': '5'
+    },
+    {
+      id: 2,
+      'sku': '02',
+      'name': 'Banana',
+      'category': 'fruit',
+      'imageThumbnail': 'http://placehold.it/32x32',
+      'imageMedium': 'http://placehold.it/300x200',
+      'imageDetail': 'http://placehold.it/600x400',
+      'price': '5'
+    },
+    {
+      id: 3,
+      'sku': '03',
+      'name': 'Tomato',
+      'category': ['fruit', 'vegetable'],
+      'imageThumbnail': 'http://placehold.it/32x32',
+      'imageMedium': 'http://placehold.it/300x200',
+      'imageDetail': 'http://placehold.it/600x400',
+      'price': '5'
+    },
+    {
+      id: 4,
+      'sku': '04',
+      'name': 'Cucumber',
+      'category': ['fruit', 'vegetable'],
+      'imageThumbnail': 'http://placehold.it/32x32',
+      'imageMedium': 'http://placehold.it/300x200',
+      'imageDetail': 'http://placehold.it/600x400',
+      'price': '5'
+    },
+    {
+      id: 5,
+      'sku': '05',
+      'name': 'Potato',
+      'category': 'vegetable',
+      'imageThumbnail': 'http://placehold.it/32x32',
+      'imageMedium': 'http://placehold.it/300x200',
+      'imageDetail': 'http://placehold.it/600x400',
+      'price': '5'
+    },
+    {
+      id: 6,
+      'sku': '06',
+      'name': 'Beet',
+      'category': 'vegetable',
+      'imageThumbnail': 'http://placehold.it/32x32',
+      'imageMedium': 'http://placehold.it/300x200',
+      'imageDetail': 'http://placehold.it/600x400',
+      'price': '5'
+    },
+    {
+      id: 7,
+      'sku': '07',
+      'name': 'Kale',
+      'category': 'vegetable',
+      'imageThumbnail': 'http://placehold.it/32x32',
+      'imageMedium': 'http://placehold.it/300x200',
+      'imageDetail': 'http://placehold.it/600x400',
+      'price': '5'
+    },
+    {
+      id: 8,
+      'sku': '08',
+      'name': 'Blueberry',
+      'category': 'fruit',
+      'imageThumbnail': 'http://placehold.it/32x32',
+      'imageMedium': 'http://placehold.it/300x200',
+      'imageDetail': 'http://placehold.it/600x400',
+      'price': '5'
+    },
+    {
+      id: 9,
+      'sku': '09',
+      'name': 'Celery',
+      'category': 'vegetable',
+      'imageThumbnail': 'http://placehold.it/32x32',
+      'imageMedium': 'http://placehold.it/300x200',
+      'imageDetail': 'http://placehold.it/600x400',
+      'price': '5'
+    },
+    {
+      id: 10,
+      'sku': '10',
+      'name': 'Orange',
+      'category': 'fruit',
+      'imageThumbnail': 'http://placehold.it/32x32',
+      'imageMedium': 'http://placehold.it/300x200',
+      'imageDetail': 'http://placehold.it/600x400',
+      'price': '5'
+    }
+  ]
 });
